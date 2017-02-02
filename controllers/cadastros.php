0c6a65318a907d2fe8ce65e310f351eb54d25b96
cachareg <cachareg@gmail.com>
2017-02-02 15:50:50 -0200

    controllers frontend

--- controllers/cadastros.php
+++ controllers/cadastros.php
@@ -1,20 +1,33 @@
 <?php
 header('Content-type:application/json');
 include "../dao/DAOProduto.php";
-
-header('Content-type:application/json');
-$dao = new DAOProduto();
-$produtos = $dao->getAllProdutos();
-echo json_encode($produtos);
+include "../dao/DAOCaracteristicas.php";
+require_once('../model/Produto.php'); 
 
 if(isset($_GET['action'])) {
     if($_GET['action'] == 'getAllProdutos') {
     	header('Content-type:application/json');
 	    $dao = new DAOProduto();
-	    $produtos = $dao->getAllProdutos();
+	    $produtos = $dao->getAllProdutos(null, null);
 		echo json_encode($produtos);
+    }
+    if($_GET['action'] == 'getAllProdutosAtivosPorCategoriasSubcategorias') {
+    	$categorias= json_decode($_GET['categorias']); 
+    	$subcategorias= json_decode($_GET['subcategorias']); 
+   		$dao = new DAOProduto();
+   		$produtos = $dao->getAllProdutos($categorias,$subcategorias );
+   		
+   		for($i=0;$i<count($produtos); $i++){
+   			if($produtos[$i]->getAtivo()==0){
+   				unset($produtos[$i]);
+   			}
+   		}
+   		echo json_encode($produtos);
+    }
+    if($_GET['action'] == 'getCaracteristicasPorProduto') {
+    	$prod= $_GET['id'];
+   		$dao = new DAOCaracteristicas();
+   		$caracteristicas = dao->getCaracteristicasPorProduto($categorias,$subcategorias );
     }
 }
 
-
-?> 
